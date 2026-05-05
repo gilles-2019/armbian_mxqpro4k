677d03d0478318b4583afd9b28d66014e70257fb
Gilles <grosminou2019@hotmail.com>
2026-05-05 11:20:18 -0400

    wip

--- extensions/t95max-uboot-signing.sh
+++ extensions/t95max-uboot-signing.sh
@@ -3,17 +3,17 @@
 : << 'COMMENTAIRE'
 # Fonction qui contient votre logique de signature FIP
 function t95max_sign_uboot_fip() {
-    display_alert "Extension" "FN Signature FIP." "info"
+    display_alert "FnFipSign" "FN Signature FIP." "info"
     # Vérification du contexte pour ne pas polluer d'autres builds
     [[ $BOARD != "sei610" && $BOARD != "t95max" ]] && return
 
-    display_alert "Extension" "Signature FIP pour S905X3..." "info"
+    display_alert "FnFipSign" "Signature FIP pour S905X3..." "info"
     export OBJ="$SRC/cache/sources/u-boot-worktree"
     local fip_tools="./userpatches/amlogic-boot-fip"
     local raw_uboot="$OBJ/u-boot/v2024.07/u-boot.bin"
     local output_dir="$OBJ/u-boot/v2024.07"
  
-    display_alert "Extension" "repertoire OBJ=<$OBJ> UBOOT_OUT=<$UBOOT_OUT> SRC=<$SRC> DEST=<$DEST>" "info"
+    display_alert "FnFipSign" "repertoire OBJ=<$OBJ> UBOOT_OUT=<$UBOOT_OUT> SRC=<$SRC> DEST=<$DEST>" "info"
      
     # Récupération des outils si nécessaire
     if [ ! -d "$fip_tools" ]; then
@@ -35,26 +35,32 @@ function t95max_sign_uboot_fip() {
     ./build-fip.sh sei610 "$raw_uboot" "$output_dir"
     
     # Préparation du chainloader pour la partition /boot
-    display_alert "Extension" "chainloader...$output_dir/u-boot.bin.sd.bin VERS $SDCARD/u-boot.ext " "info"
+    display_alert "FnFipSign" "chainloader...$output_dir/u-boot.bin.sd.bin VERS $SDCARD/u-boot.ext " "info"
     cp "$output_dir/u-boot.bin.sd.bin" "$SDCARD/u-boot.ext" 2>/dev/null || true
 
-    display_alert "Extension" "Signature terminée : u-boot.bin.sd.bin généré" "success"
+    display_alert "FnFipSign" "Signature terminée : u-boot.bin.sd.bin généré" "success"
 }
 COMMENTAIRE
 
 
 function post_build_uboot() {
     # On ne cible que la board sei610 (chipset SM1 / S905X3)
-    display_alert "Extension" "FN Signature FIP." "info"
+    display_alert "FnFipSign" "FN Signature FIP." "info"
     [[ $BOARD != "sei610" ]] && return
 
     display_alert "S905X3" "Début de la signature FIP pour chainloader" "info"
-
+    display_alert "FnFipSign" "repertoire  SRC=<$SRC> DEST=<$DEST> " "info"
     # 1. Définition des chemins
     local uboot_dir="$(pwd)"
+# cache/sources/amlogic-boot-fip/sei610
     local fip_src="$SRC/userpatches/amlogic-boot-fip" # Dossier où vous avez cloné les blobs
     local board_fip="$fip_src/sei610"
     local output_bin="$DEST/u-boot.ext"
+    
+    display_alert "FnFipSign" "local var  uboot_dir=<$SRC> " "info"
+    display_alert "FnFipSign" "local var  fip_src=<$SRC> " "info"
+    display_alert "FnFipSign" "local var  board_fip=<$SRC> " "info"
+    display_alert "FnFipSign" "local var  output_bin=<$SRC> " "info"
 
     # 2. Vérification de la présence des outils et blobs
     if [ ! -d "$board_fip" ]; then
@@ -102,14 +108,14 @@ function post_build_uboot() {
 # ENREGISTREMENT DU HOOK
 # Argument 1 : Le point d'ancrage (étape du build)
 # Argument 2 : Le nom de votre fonction ci-dessus
-display_alert "Extension" "ENREGISTREMENT DU HOOK" "info"
+display_alert "FnFipSign" "ENREGISTREMENT DU HOOK" "info"
 # À la fin de votre script .sh
 if  type add_hook >/dev/null 2>&1; then
-    display_alert "Extension" "ADD HOOK" "info"
+    display_alert "FnFipSign" "ADD HOOK" "info"
     add_hook "post_build_uboot" "t95max_sign_uboot_fip"
 else
     # Si add_hook n'est pas là, on définit la fonction standard en secours
-    display_alert "Extension" "ADD HOOK fonction standard en secours" "info"
+    display_alert "FnFipSign" "ADD HOOK fonction standard en secours" "info"
     function uboot_custom_postprocess { t95max_sign_uboot_fip; }
 fi
 COMMENTAIRE
