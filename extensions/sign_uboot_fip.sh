7aaaafc1d7b12c80f4dfa4cd53cd1ec7d3d591d7
Gilles <grosminou2019@hotmail.com>
2026-05-06 17:02:35 -0400

    wip

--- extensions/sign_uboot_fip.sh
+++ extensions/sign_uboot_fip.sh
@@ -1,4 +1,7 @@
-
+post_uboot_custom_postprocess__hook_meson_sm1() {
+	display_alert "GILLES" "trace dans ${FUNCNAME[0]} *** appel fn de meson_sm1.conf" "info"
+	uboot_custom_postprocess
+}
 post_uboot_custom_postprocess_NOTDONE_hook_sign_amlogic_fip() {
     display_alert "GILLES" "trace dans ${FUNCNAME[0]} *** signature du uboot" "info"
     display_alert "sign_amlogic_fip() BOARD_NAME=$BOARD_NAME BOARD=$BOARD" "Amlogic-Secure" "info"
